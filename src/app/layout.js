f5b7258c6bc7717efcfe8fa32456be108770aa60
vedantsen905 <120441464+vedantsen905@users.noreply.github.com>
2025-07-21 10:14:05 +0530

    logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* Favicon (will be shown in browser tab) */}
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/logo.png" />
 
         {/* Title (text shown beside favicon in tab) */}
         <title>FGIS - FG Life Insurance</title>
